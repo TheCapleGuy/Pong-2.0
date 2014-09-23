f6f49df4c3f3c69cdfc632e0199e5040930afd48
2intheBush <adrianocaple@gmail.com>
2014-09-23 09:16:47 -0700

    final save

--- source/main.cpp
+++ source/main.cpp
@@ -1,27 +1,39 @@
 ﻿#include "AIE.h"
 #include <iostream>
 #include "time.h"
+#include <fstream>
+
+using namespace std;
 
 void UpdateGameState(float a_DeltaTime);
 
-//Scoring Function
-void UpdateScoring();
+// My read me file from a I/O file
+void readme();
 
 enum GAMESTATES{
 	MAINMENU,
 	GAMEPLAY,
 	HOWTOPLAY,
-	END,
+	HIGHSCORES,
 };
 enum BALLDIRECT{
 	UP,
 	DOWN,
 };
+enum ROUNDSTATE{
+	SERVE,
+	ROUND,
+	ONEWINS,
+	TWOWINS,
+};
 
-BALLDIRECT GetRandY();
+
 
 //Enum States
 BALLDIRECT upDownCurrent;
+BALLDIRECT GetRandY();
+ROUNDSTATE gameState;
+
 
 //global variables
 const int screenWidth = 800;
@@ -29,6 +41,7 @@ const int screenHeight = 600;
 
 class Paddle{
 public:
+
 
 	unsigned int spriteID;
 	unsigned int Score;
@@ -74,6 +87,16 @@ public:
 		}
 		MoveSprite(spriteID, x, y);
 	}
+	unsigned int score = 0;
+	void AddScore(unsigned int a_score)
+	{
+		score += a_score;
+	}
+
+	void GetScore(char* a_result)
+	{
+		itoa(score, a_result, 10);
+	}
 
 
 };
@@ -105,7 +128,7 @@ public:
 		y = a_y;
 	}
 
-	
+
 	void SetSize(unsigned int a_bWidth, unsigned int a_bHieght){
 		bWidth = a_bWidth;
 		bHeight = a_bHieght;
@@ -116,17 +139,17 @@ public:
 		moveUpExt = a_moveUpExt;
 		moveDownExt = a_moveDownExt;
 	}
-	
+
 
 	void SetLeftRightExt(unsigned int a_moveRightExt, unsigned int a_moveLeftExt){
 		moveRightExt = a_moveRightExt;
 		moveLeftExt = a_moveLeftExt;
 	}
 
-	
-	
+
+
 	void UpDownMove(float a_fTimeStep, float a_bGravity){
-		
+
 		switch (upDownCurrent){
 		case UP:
 			if (y >= moveUpExt){
@@ -135,7 +158,7 @@ public:
 			else {
 				y += a_bGravity * a_fTimeStep;
 			}
-		break;
+			break;
 		case DOWN:
 			if (y <= moveDownExt){
 				upDownCurrent = UP;
@@ -144,14 +167,14 @@ public:
 			else {
 				y -= a_bGravity * a_fTimeStep;
 			}
-		break;
+			break;
 		}
 	}
 
 	void LeftRightMove(float a_fTimeStep, float a_bSpeed){
-	
+
 		x += bSpeed * a_fTimeStep;
-		              
+
 	}
 
 	bool CollideOne(){
@@ -164,11 +187,11 @@ public:
 		}
 		else{
 			return false;
-		} 
+		}
 	}
-	
+
 	bool CollideTwo(){
-		if (x + bWidth * .5f >= playerTwo.x - playerTwo.pWidth * .5f && 
+		if (x + bWidth * .5f >= playerTwo.x - playerTwo.pWidth * .5f &&
 			y - bHeight * .5f <= playerTwo.y + playerTwo.pHeight * .5f&&
 			y + bHeight * .5f >= playerTwo.y - playerTwo.pHeight * .5f){
 			while (x <= playerTwo.x){
@@ -179,16 +202,13 @@ public:
 			return false;
 		}
 	}
-
-	void Serve(){
-
-	}
-
 };
 Ball GameBall;
 
-int main( int argc, char* argv[] )
-{	
+int mHighScores;
+
+int main(int argc, char* argv[])
+{
 	bool runGame = true;
 	srand(time(nullptr));
 	upDownCurrent = GetRandY();
@@ -208,7 +228,7 @@ int main( int argc, char* argv[] )
 	playerTwo.Score = 0;
 
 	GameBall.SetPosition(screenWidth * .5f, screenHeight * .5f);
-	GameBall.SetSize(18,18);
+	GameBall.SetSize(18, 18);
 	int randXDirection;
 	int i = rand() % 2;
 	if (i == 1){
@@ -224,28 +244,31 @@ int main( int argc, char* argv[] )
 
 
 	Initialise(screenWidth, screenHeight, false, "Fucking Pong");
-    SetBackgroundColour(SColour(0, 0, 0, 255));
+	SetBackgroundColour(SColour(0, 0, 0, 255));
 
-	playerOne.spriteID = CreateSprite("./images/PlayerPaddle.png",playerOne.pWidth, playerOne.pHeight, true);
+	playerOne.spriteID = CreateSprite("./images/PlayerPaddle.png", playerOne.pWidth, playerOne.pHeight, true);
 	playerTwo.spriteID = CreateSprite("./images/PlayerPaddle.png", playerTwo.pWidth, playerTwo.pHeight, true);
 	GameBall.spriteID = CreateSprite("./images/PlayerPaddle.png", GameBall.bWidth, GameBall.bHeight, true);
-	
+
 	GAMESTATES currentState = MAINMENU;
 
 	//Game Loop
-    do
-    {
+	do
+	{
 		float deltaT = GetDeltaTime();
 		switch (currentState){
 		case MAINMENU:
 			DrawString("Welcome To Pong", screenWidth * .5f, screenHeight *.5f);
 			DrawString("Press Enter To Sart Game", screenWidth * .5f, 260);
 			DrawString("If you dont know how to play...", screenWidth * .5f, 220);
-			DrawString("Your dumb.", screenWidth * .5f, 180);
+			DrawString("You're dumb.", screenWidth * .5f, 180);
 			DrawString("Press Q to learn how", screenWidth * .5f, 140);
 			DrawString("Press Esc to Quit", screenWidth * .5f, 100);
 			DrawString("Press H For HighScores", screenWidth * .5f, 60);
 
+			if (IsKeyDown(72)){
+				currentState = HIGHSCORES;
+			}
 			if (IsKeyDown(257)){
 				currentState = GAMEPLAY;
 			}
@@ -260,14 +283,69 @@ int main( int argc, char* argv[] )
 			ClearScreen();
 			break;
 		case GAMEPLAY:
-			
-			GameBall.bSpeed + 100.f;
+			GameBall.bSpeed + 50.f;
 			ClearScreen();
+
+			//Drawing the Score
+			char playerOneScore[3];
+			char playerTwoScore[3];
+			playerOne.GetScore(playerOneScore);
+			playerTwo.GetScore(playerTwoScore);
+			DrawString(playerOneScore, 100, 500);
+			DrawString(playerTwoScore, 700, 500);
+			if (playerOne.score >= 10){
+				gameState = ONEWINS;
+			}
+			if (playerTwo.score >= 10){
+				gameState = TWOWINS;
+
+			}
+			switch (gameState){
+			case SERVE:
+				GameBall.bSpeed *= -1.f;
+				GameBall.x = screenWidth * .5f;
+				GameBall.y = screenHeight * .5f;
+				gameState = ROUND;
+				break;
+			case ROUND:
+				if (GameBall.x <= 0){
+					playerTwo.AddScore(1);
+					gameState = SERVE;
+				}
+				if (GameBall.x >= screenWidth){
+					playerOne.AddScore(1);
+					gameState = SERVE;
+				}
+				break;
+			case ONEWINS:
+				ClearScreen();
+				DrawString("Player One WINS", screenWidth * .35f, screenHeight * .5f);
+				DrawString("Backspace to go to the menu", screenWidth * .3f, screenHeight * .4f);
+				mHighScores = playerOne.score
+					;
+				if (IsKeyDown(259)){
+					currentState = MAINMENU;
+				}
+				break;
+			case TWOWINS:
+				ClearScreen();
+				DrawString("Player Two WINS", screenWidth * .35f, screenHeight * .5f);
+				DrawString("Backspace to go to the menu", screenWidth * .3f, screenHeight * .4f);
+				mHighScores = playerTwo.score;
+				if (IsKeyDown(259)){
+					currentState = MAINMENU;
+				}
+				break;
+			default:
+				break;
+			}
+
 			UpdateGameState(deltaT);
 			DrawSprite(playerOne.spriteID);
 			DrawSprite(playerTwo.spriteID);
 			DrawSprite(GameBall.spriteID);
-			
+
+
 			if (IsKeyDown(256)){
 				runGame = false;
 			}
@@ -280,24 +358,29 @@ int main( int argc, char* argv[] )
 			DrawString("player Two uses I and K to move his character", screenWidth * .05f, 420);
 			DrawString("Press Spacebar to go back to the main menu", screenWidth * .05f, 140);
 
-			
+
 
 			if (IsKeyDown(32)){
 				currentState = MAINMENU;
 			}
 			break;
+		case HIGHSCORES:
+			char buff[30];
+			DrawString(itoa(mHighScores, buff, 10), screenWidth * .15f, screenHeight * .5f);
+			if (IsKeyDown(257)){
+				currentState = MAINMENU;
+			}
 		}
-    } while((!FrameworkUpdate()) && runGame == true);
+	} while ((!FrameworkUpdate()) && runGame == true);
 
-    Shutdown();
+	Shutdown();
 
 
-    return 0;
+	return 0;
 }
 
 void UpdateGameState(float a_DeltaTime){
-	
-	UpdateScoring();
+
 	//Collision check in game
 	if (GameBall.bSpeed > 0){
 		if (GameBall.CollideTwo() == true){
@@ -316,12 +399,6 @@ void UpdateGameState(float a_DeltaTime){
 	GameBall.UpDownMove(a_DeltaTime, GameBall.bGravity);
 }
 
-void UpdateScoring(){
-	if (GameBall.x <= GameBall.moveLeftExt){
-
-	}
-}
-
 BALLDIRECT GetRandY(){
 	int i = rand() % 2;
 	if (i == 1){
@@ -331,3 +408,22 @@ BALLDIRECT GetRandY(){
 		return DOWN;
 	}
 }
+
+void WriteHighScore()
+{
+	fstream Highscores;
+	Highscores.open("Highscores.txt", ios::out);
+	Highscores << mHighScores;
+	Highscores.close();
+}
+
+void LoadHighScore()
+{
+	fstream Highscores;
+	Highscores.open("Highscores.txt", ios::in);
+	char buffer[10];
+	Highscores.getline(buffer, 10);
+	mHighScores = atoi(buffer);
+	Highscores.close();
+}
+
